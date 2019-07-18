80c3f2e12e64fde94d24fd73a2e4c02622c9e999
garto <elgartoinf@gmail.com>
2019-07-17 20:53:12 -0500

    added platforms

--- juego.py
+++ juego.py
@@ -53,10 +53,19 @@ def showInfinityBackground():
         pantalla.blit(pygame.transform.scale(fondo, (ANCHO, ALTO)), (backgroundPosX+ANCHO, 0))
 
 
+class Bloque (pygame.sprite.Sprite):
+    def __init__(self, p, dims):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.Surface(dims)
+        self.image.fill(VERDE)
+        self.rect=self.image.get_rect()
+        self.rect.x=p[0]
+        self.rect.y=p[1]
+
 class Jugador (pygame.sprite.Sprite):
     def __init__(self, p):
         pygame.sprite.Sprite.__init__(self)
-        self.m = corte('personaje.png', 5,4)
+        self.m = corte('personaje.png', 8,2)
         self.image = self.m[0][0][0]
         self.rect=self.image.get_rect()
         self.rect.x=p[0]
@@ -66,14 +75,20 @@ class Jugador (pygame.sprite.Sprite):
         self.vidas=3
         self.siguiente = "d"
         self.sig = 0
+        self.plataformas=None
         
+    def gravedad(self, cte=0.5):
+        if self.vely == 0:
+            self.vely = 1
+        else:
+            self.vely += cte
 
     def update(self):
 
         if self.siguiente == "d":
             self.image = self.m[0][0][0+self.sig]
         if self.siguiente == "i":
-            self.image = self.m[0][0][0+self.sig]
+            self.image = self.m[0][1][0+self.sig]
         if self.siguiente == "a":
             self.image = self.m[0][0][0+self.sig]
         if self.siguiente == "b":
@@ -81,16 +96,40 @@ class Jugador (pygame.sprite.Sprite):
 
 
         self.sig = self.sig + 1
-        if self.sig == 2:
+        if self.sig == 5:
             self.sig = 0
-            
 
 
         if self.velx != 0:
-            if self.rect.x < (ANCHO/2+100):
+            if self.rect.x <= (ANCHO-200) and self.rect.x >= 0:
+                print(self.velx)
                 self.rect.x += self.velx
-        if self.vely != 0:
-            self.rect.y += self.vely
+            else:
+                if self.rect.x >= 0:
+                    self.rect.x -= 50
+                else:
+                    self.rect.x += 50
+        
+
+
+
+        self.rect.y += self.vely
+        
+
+        ls_col=pygame.sprite.spritecollide(self,self.plataformas,False)
+        for p in ls_col:
+            if self.rect.top < p.rect.bottom and self.vely < 0:
+                self.rect.top = p.rect.bottom + 10
+                self.vely = 0
+            if self.rect.bottom > p.rect.top and self.vely > 0:
+                self.rect.bottom = p.rect.top + 10
+                self.vely = 0
+
+        if self.rect.bottom > ALTO:
+            self.rect.bottom=ALTO
+            self.vely=0
+
+        self.gravedad()
 
 
 
@@ -99,35 +138,39 @@ if __name__ == '__main__':
     pantalla = pygame.display.set_mode([ANCHO,ALTO])
 
     jugadores = pygame.sprite.Group()
-    j=Jugador([0,ALTO-90])
+
+    j=Jugador([0,ALTO-120])
     jugadores.add(j)    
+
+
+    plataformas= pygame.sprite.Group()
+
+    p=Bloque([150,100],[120,20])
+    plataformas.add(p)
+
+    p=Bloque([1300,200],[120,20])
+    plataformas.add(p)
+
+    j.plataformas=plataformas
+
     reloj=pygame.time.Clock()
-
     fin=False
+    cont = 0
     while not fin:
-        contador = 0
-        segundos=contador//60
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin=True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     j.siguiente = "d"
-                    j.velx=5
+                    j.velx=10
                     j.vely=0
                 if event.key == pygame.K_LEFT:
                     j.siguiente = "i"
-                    j.velx= -5
+                    j.velx= -10
                     j.vely=0
-                if event.key == pygame.K_UP:
-                    j.siguiente = "a"
-                    j.vely=-5
-                    j.velx=0
-                if event.key == pygame.K_DOWN:
-                    j.siguiente = "b"
-                    j.vely=5
-                    j.velx=0
+                if event.key == pygame.K_SPACE:
+                    j.vely= -10
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     j.velx=0
@@ -142,11 +185,19 @@ if __name__ == '__main__':
                     j.velx=0
                     j.vely=0
 
+        if j.rect.x <= (ANCHO-200) and j.rect.x >= 0:
+            for p in plataformas:
+                p.rect.x+=j.velx
+                print(p.rect.x,j.velx)
 
-        velX = (j.velx/2)
-        jugadores.update()
+        velX = (j.velx/4)
+        if (cont % 4) == 0:
+            jugadores.update()
         showInfinityBackground()
         jugadores.draw(pantalla)
+        plataformas.draw(pantalla)
         pygame.display.flip()
-        reloj.tick(segundos)
-        contador+=1
+
+        cont+=1
+        if cont == 60:
+            cont = 0
